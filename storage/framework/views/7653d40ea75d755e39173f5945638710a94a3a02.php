e7955e4aefaa06f3ece0a3413171e040ae90920c
rohit82drj <rohitrajput.drj@gmail.com>
2021-10-28 10:18:49 +0530

    header sequencing chnage

--- storage/framework/views/7653d40ea75d755e39173f5945638710a94a3a02.php
+++ storage/framework/views/7653d40ea75d755e39173f5945638710a94a3a02.php
@@ -37,7 +37,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
 	<meta charset="utf-8" />
 
-	<title>{{ App\Models\GeneralSetting::where('deleted_at',NULL)->value('site_name') }}</title>
+	<title><?php echo e(App\Models\GeneralSetting::where('deleted_at',NULL)->value('site_name')); ?></title>
 
 	<meta name="description" content="Latest updates and statistic charts">
 
@@ -53,11 +53,11 @@ License: You must have a valid license purchased only from themeforest(the above
 	<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css" />
 	<link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.1/css/fixedHeader.dataTables.min.css" />
 	<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.0.2/css/responsive.dataTables.min.css" /> -->
-	<link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+	<link href="<?php echo e(asset('assets/plugins/custom/datatables/datatables.bundle.css')); ?>" rel="stylesheet" type="text/css" />
 
 	<!--begin::Page Vendors Styles(used by this page) -->
 
-	<link href="{{ asset('/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet" type="text/css" />
+	<link href="<?php echo e(asset('/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')); ?>" rel="stylesheet" type="text/css" />
 
 
 
@@ -67,17 +67,17 @@ License: You must have a valid license purchased only from themeforest(the above
 
 	<!--begin::Global Theme Styles(used by all pages) -->
 
-	<link href="{{ asset('/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+	<link href="<?php echo e(asset('/assets/plugins/global/plugins.bundle.css')); ?>" rel="stylesheet" type="text/css" />
 
-	<link href="{{ asset('/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+	<link href="<?php echo e(asset('/assets/css/style.bundle.css')); ?>" rel="stylesheet" type="text/css" />
 
 
 
 	<!--end::Global Theme Styles -->
 
-	<script src="{{ asset('/assets/plugins/global/plugins.bundle.js') }}" type="text/javascript"></script>
+	<script src="<?php echo e(asset('/assets/plugins/global/plugins.bundle.js')); ?>" type="text/javascript"></script>
 
-	<script src="{{ asset('/assets/js/scripts.bundle.js') }}" type="text/javascript"></script>
+	<script src="<?php echo e(asset('/assets/js/scripts.bundle.js')); ?>" type="text/javascript"></script>
 
 	<script src="https://momentjs.com/downloads/moment.js" type="text/javascript"></script>
 
@@ -89,17 +89,17 @@ License: You must have a valid license purchased only from themeforest(the above
 
 	
 
-	@php $icon = App\Models\GeneralSetting::where('deleted_at',NULL)->value('favicon_icon'); @endphp
+	<?php $icon = App\Models\GeneralSetting::where('deleted_at',NULL)->value('favicon_icon'); ?>
 
-	@if(!empty($icon))
-		<link rel="shortcut icon" href="{{asset('/storage/uploads/brand/'.$icon)}}" />
-	@else
-		<link rel="shortcut icon" href="{{asset('assets/media/logos/buzzed_logo.png')}}" />
-	@endif
+	<?php if(!empty($icon)): ?>
+		<link rel="shortcut icon" href="<?php echo e(asset('/storage/uploads/brand/'.$icon)); ?>" />
+	<?php else: ?>
+		<link rel="shortcut icon" href="<?php echo e(asset('assets/media/logos/buzzed_logo.png')); ?>" />
+	<?php endif; ?>
 
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-	@stack('styles')
+	<?php echo $__env->yieldPushContent('styles'); ?>
 
 </head>
 
@@ -125,9 +125,9 @@ License: You must have a valid license purchased only from themeforest(the above
 
 		<div class="kt-header-mobile__logo">
 
-			<a href="{{route('admin.home')}}">
+			<a href="<?php echo e(route('admin.home')); ?>">
 
-				<img alt="Logo" src="{{asset('assets/media/logos/buzzed_logo.png')}}" style="height: 50px; width: 50px;" />
+				<img alt="Logo" src="<?php echo e(asset('assets/media/logos/buzzed_logo.png')); ?>" style="height: 50px; width: 50px;" />
 
 			</a>
 
@@ -169,26 +169,26 @@ License: You must have a valid license purchased only from themeforest(the above
 					<div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500">
 
 						<ul class="kt-menu__nav ">
-							@if(Auth::guard('vendor')->check())
-								<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('vendor.product')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-box"></i><span class="kt-menu__link-text">Product</span></a></li>
+							<?php if(Auth::guard('vendor')->check()): ?>
+								<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('vendor.product')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-box"></i><span class="kt-menu__link-text">Product</span></a></li>
 
-								<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('vendor.revenue.index')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-percent"></i><span class="kt-menu__link-text">Commision Report</span></a></li>
+								<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('vendor.revenue.index')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-percent"></i><span class="kt-menu__link-text">Commision Report</span></a></li>
 
-							@endif
+							<?php endif; ?>
 
-							@if(Auth::guard('admin')->check())
+							<?php if(Auth::guard('admin')->check()): ?>
 
-							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('admin.bookingstatus.index')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-eye"></i><span class="kt-menu__link-text">Booking Status</span></a></li>
+							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('admin.bookingstatus.index')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-eye"></i><span class="kt-menu__link-text">Booking Status</span></a></li>
 
-							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('admin.category.index')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-layer-group"></i><span class="kt-menu__link-text">Category</span></a></li>
+							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('admin.category.index')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-layer-group"></i><span class="kt-menu__link-text">Category</span></a></li>
 
-							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('admin.commision.index')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-money-bill-wave"></i><span class="kt-menu__link-text">Commision</span></a></li>
+							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('admin.commision.index')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-money-bill-wave"></i><span class="kt-menu__link-text">Commision</span></a></li>
 
-							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('admin.vendors.index','all')}}" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-users"></i><span class="kt-menu__link-text">Customer</span></a></li>
+							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('admin.vendors.index','all')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fas fa-users"></i><span class="kt-menu__link-text">Customer</span></a></li>
 
-							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="{{route('admin.service.index')}}" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-atom"></i><span class="kt-menu__link-text">Service</span></a></li>
+							<li class="kt-menu__item  kt-menu__item--active" aria-haspopup="true"><a href="<?php echo e(route('admin.service.index')); ?>" class="kt-menu__link "><i class="kt-menu__link-icon fa fa-atom"></i><span class="kt-menu__link-text">Service</span></a></li>
 												
-							@endif
+							<?php endif; ?>
 						</ul>
 
 					</div>
@@ -221,14 +221,14 @@ License: You must have a valid license purchased only from themeforest(the above
 
 						<div class="kt-header__brand-logo">
 
-							<a href="{{route('admin.home')}}">
-								@php $logo = App\Models\GeneralSetting::where('deleted_at',NULL)->value('logo'); @endphp
+							<a href="<?php echo e(route('admin.home')); ?>">
+								<?php $logo = App\Models\GeneralSetting::where('deleted_at',NULL)->value('logo'); ?>
 
-								@if(!empty($logo))
-									<img alt="Logo" src="{{asset('/storage/uploads/brand/'.$logo)}}" style="height: 80px; width: 100px;" />
-								@else
-									<img alt="Logo" src="{{asset('assets/media/logos/buzzed_logo.png')}}" style="height: 80px; width: 100px;" />
-								@endif
+								<?php if(!empty($logo)): ?>
+									<img alt="Logo" src="<?php echo e(asset('/storage/uploads/brand/'.$logo)); ?>" style="height: 80px; width: 100px;" />
+								<?php else: ?>
+									<img alt="Logo" src="<?php echo e(asset('assets/media/logos/buzzed_logo.png')); ?>" style="height: 80px; width: 100px;" />
+								<?php endif; ?>
 
 							</a>
 
@@ -245,8 +245,9 @@ License: You must have a valid license purchased only from themeforest(the above
 					<!-- begin:: Title -->
 
 					<h3 class="kt-header__title kt-grid__item">
-						<a href="{{route('admin.home')}}" style="color: #00c5ff;">
-							{{ App\Models\GeneralSetting::where('deleted_at',NULL)->value('site_name') }}
+						<a href="<?php echo e(route('admin.home')); ?>" style="color: #00c5ff;">
+							<?php echo e(App\Models\GeneralSetting::where('deleted_at',NULL)->value('site_name')); ?>
+
 						</a>
 					</h3>
 
@@ -265,7 +266,7 @@ License: You must have a valid license purchased only from themeforest(the above
 						<div id="kt_header_menu" class="kt-header-menu kt-header-menu-mobile  kt-header-menu--layout-default ">
 
 							<ul class="kt-menu__nav ">
-								<!-- <li class="kt-menu__item  kt-menu__item--active " aria-haspopup="true"><a href="{{route('admin.home')}}" class="kt-menu__link "><span class="kt-menu__link-text">Dashboard</span></a></li> -->
+								<!-- <li class="kt-menu__item  kt-menu__item--active " aria-haspopup="true"><a href="<?php echo e(route('admin.home')); ?>" class="kt-menu__link "><span class="kt-menu__link-text">Dashboard</span></a></li> -->
 
 							</ul>
 
@@ -275,4 +276,4 @@ License: You must have a valid license purchased only from themeforest(the above
 
 
 
-					<!-- end: Header Menu -->
+					<!-- end: Header Menu --><?php /**PATH D:\rohit\service_provider\service_provider\resources\views/admin/layout/header.blade.php ENDPATH**/ ?>
